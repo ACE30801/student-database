c2f6370215209ef7fba6b510d62a191a552cb2b3
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2021-06-08 15:31:53 +0300

    Insert function

--- index.c
+++ index.c
@@ -11,6 +11,7 @@
  char email[30];
  char Birthday[30];
  struct sub *subjects[4];
+ int totalmark;
  struct list_infostudent *nextptr;
  }studl;
 
@@ -23,6 +24,7 @@
  char email[30];
  char Birthday[30];
  struct sub *subjects[4];
+ int totalmark;
  }stud;
 
  struct sub{
@@ -32,70 +34,195 @@
 studl *head = NULL;
 studl *curr = NULL;
 
-void start(char name[50], char currentyear[15],
-int section, int code, long long int national_id,
-char email[30], char Birthday[30], struct sub *subjects[4]);
+void start();
+void insert();
+
 
 int main()
 {
-stud s;
-studl sl;
-FILE *pfile;
-pfile = fopen("data.txt","rb");
-if(pfile==NULL)
-    {
-    printf("couldn't find the file\n");
-    }
-else{
-    printf("successfully found the file\n");
-    while(fread(&s, sizeof(s), 1, pfile)==1){
-start(s.name, s.currentyear, s.section,
-s.code, s.national_id,s.email,
-s.Birthday, s.subjects);
- printf("yes\t");
-        }
-    }
-    fclose(pfile);
 
-    curr = head;
-    pfile = fopen("data.txt","wb");
-while(curr!=NULL){
-    strcpy(s.name, curr->name);
-    strcpy(s.currentyear, curr->currentyear);
-    s.section = curr->section;
-    s.code = curr->code;
-    s.national_id = curr->national_id;
-    strcpy(s.email, curr->email);
-    strcpy(s.Birthday, curr->Birthday);
-    for(int i=0;i<=10;i++){
-       s.subjects[i] = curr->subjects[i];
-    }
 
-    }
-
-    return 0;
-}
-void start(char name[50], char currentyear[15],
-int section, int code, long long int national_id,
-char email[30], char Birthday[30], struct sub *subjects[4])
-{
-    studl *std = (studl *)malloc(sizeof(studl));
-    strcpy(std->name, name);
-    strcpy(std->currentyear, currentyear);
-    std->section = section;
-    std->code = code;
-    std->national_id = national_id;
-    strcpy(std->email, email);
-    strcpy(std->Birthday, Birthday);
-    for(int i=0;i<=10;i++){
-       std->subjects[i] = subjects[i];
-    }
-    std->nextptr = NULL;
-    if (head == NULL)head = std;
-    else {
-        studl *temp = head;
-        while (temp->nextptr != NULL)temp = temp->nextptr;
-        temp->nextptr = std;
-    }
     return;
 }
+
+
+
+
+void insert()
+ {
+
+    infostptr newPtr = malloc(sizeof(studl));
+
+    newPtr->subjects[0] =  malloc(sizeof(struct sub));
+    newPtr->subjects[1] =  malloc(sizeof(struct sub));
+    newPtr->subjects[2] =  malloc(sizeof(struct sub));
+    newPtr->subjects[3] =  malloc(sizeof(struct sub));
+
+    if (newPtr != NULL) {
+
+//adding all data for a new student
+
+    printf("Enter the student's name : ");
+    fflush(stdin);
+    gets(newPtr->name);
+    printf("Enter the student's code : ");
+    fflush(stdin);
+    scanf("%d",&newPtr->code);
+
+    printf("Enter the student's ID :");
+    fflush(stdin);
+    scanf("%lli",&newPtr->national_id);
+    printf("Enter the student's date of birth :");
+    fflush(stdin);
+    gets(newPtr->Birthday);
+
+    printf("Enter the student's E-mail :");
+    fflush(stdin);
+    gets(newPtr->email);
+
+    printf("Enter the student's current year :");
+    fflush(stdin);
+    gets(newPtr->currentyear);
+    printf("Enter the student's section :");
+    fflush(stdin);
+    scanf("%d",&newPtr->section);
+
+    printf("Enter [name] of the subject No.1 :");
+    fflush(stdin);
+    gets(newPtr->subjects[0]->name);
+    printf("Enter [degree] of the subject No.1 :");
+    fflush(stdin);
+    scanf("%d",&newPtr->subjects[0]->mark);
+
+    printf("Enter [name] of the subject No.2 :");
+    fflush(stdin);
+    gets(newPtr->subjects[1]->name);
+    printf("Enter [degree] of the subject No.2 :");
+    fflush(stdin);
+    scanf("%d",&newPtr->subjects[1]->mark);
+
+    printf("Enter [name] of the subject No.3 :");
+    fflush(stdin);
+    gets(newPtr->subjects[2]->name);
+    printf("Enter [degree] of the subject No.3 :");
+    fflush(stdin);
+    scanf("%d",&newPtr->subjects[2]->mark);
+
+    printf("Enter [name] of the subject No.4 :");
+    fflush(stdin);
+    gets(newPtr->subjects[3]->name);
+    printf("Enter [degree] of the subject No.4 :");
+    fflush(stdin);
+    scanf("%d",&newPtr->subjects[3]->mark);
+
+    newPtr->nextptr = NULL;
+
+
+
+    studl *previousPtr = NULL;
+    studl *currentPtr = head;
+
+
+    int record ,fname ,lname,space1 ,space2;
+    bool bigger ,lower;
+
+
+    for(record=0; currentPtr != NULL ;record++){
+            bigger=false ,lower=false;
+        for(fname=0; fname<3; fname++){
+            if(newPtr->name[fname] < currentPtr->name[fname]){
+                    lower=true; break;  }
+            else if(newPtr->name[fname] == currentPtr->name[fname]) {continue;}
+            else{ bigger=true ; break;}
+        }
+
+                if(lower){ break;}
+                if(bigger){
+                previousPtr = currentPtr;
+                currentPtr = currentPtr->nextptr;
+                    continue;}
+
+        for(space1=1; newPtr->name[space1]!=' ' ; space1++){}
+        for(space2=1; currentPtr->name[space2]!=' ' ; space2++){}
+
+        for(lname=0; lname<3; lname++, space1++,space2++){
+            if(newPtr->name[space1] < currentPtr->name[space2]){ lower=true; break;}
+            else if(newPtr->name[space1] == currentPtr->name[space2]) {continue;}
+            else{  break;}
+        }
+                if(lower){break;}
+
+                previousPtr = currentPtr;
+                currentPtr = currentPtr->nextptr;
+    }
+
+ if (previousPtr == NULL) {
+ newPtr->nextptr = head;
+ head = newPtr;
+ }
+ else {
+ previousPtr->nextptr = newPtr;
+ newPtr->nextptr = currentPtr;
+ }
+ }
+ else {
+ printf(" No memory available.\n" );
+ }
+ }
+
+
+
+
+
+void start()
+{
+
+
+     FILE *pfile1 ;
+    pfile1= fopen("data.txt","rb");
+    if(pfile1==NULL)
+    {
+        printf("couldn't find the file\n");
+        exit(1);
+    }
+
+ studl *cur = malloc(sizeof(studl));
+
+    while( 1 ){
+
+        studl *temp = malloc(sizeof(studl));
+        if( fread(&temp->name,sizeof(temp->name),1,pfile1)  == 0 ) {  break;}
+
+        temp->subjects[0] =  malloc(sizeof(struct sub));
+        temp->subjects[1] =  malloc(sizeof(struct sub));
+        temp->subjects[2] =  malloc(sizeof(struct sub));
+        temp->subjects[3] =  malloc(sizeof(struct sub));
+
+        fread(&temp->code,sizeof(temp->code),1,pfile1);
+        fread(&temp->national_id,sizeof(temp->national_id),1,pfile1);
+        fread(&temp->Birthday,sizeof(temp->Birthday),1,pfile1);
+        fread(&temp->email,sizeof(temp->email),1,pfile1);
+        fread(&temp->currentyear,sizeof(temp->currentyear),1,pfile1);
+        fread(&temp->section,sizeof(temp->section),1,pfile1);
+
+        fread(&temp->subjects[0]->name,sizeof(temp->subjects[0]->name),1,pfile1);
+        fread(&temp->subjects[0]->mark,sizeof(temp->subjects[0]->mark),1,pfile1);
+        fread(&temp->subjects[1]->name,sizeof(temp->subjects[1]->name),1,pfile1);
+        fread(&temp->subjects[1]->mark,sizeof(temp->subjects[1]->mark),1,pfile1);
+        fread(&temp->subjects[2]->name,sizeof(temp->subjects[2]->name),1,pfile1);
+        fread(&temp->subjects[2]->mark,sizeof(temp->subjects[2]->mark),1,pfile1);
+        fread(&temp->subjects[3]->name,sizeof(temp->subjects[3]->name),1,pfile1);
+        fread(&temp->subjects[3]->mark,sizeof(temp->subjects[3]->mark),1,pfile1);
+
+        if(head == NULL){head = temp; cur=temp;}
+
+        cur->nextptr=temp;
+        cur=cur->nextptr;
+
+        }
+    cur->nextptr = NULL ;
+    fclose(pfile1);
+
+ }
+
+
